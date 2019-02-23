0af17771d6599ef5ea7508b32896c5d3e308865d
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2019-02-23 20:57:15 +0900

    formula: support $/$$ surrounded TeX formula in plain text

--- src/commands.test.ts
+++ src/commands.test.ts
@@ -2,36 +2,76 @@ import { get } from 'request';
 import { promisify } from 'util';
 import { evalOnGAS } from './test-util';
 declare const slackCommands: any;
+declare const getFormulaImageURL: Function
 
-describe('slackCommands.formula', () => {
-  let res = undefined;
+describe('getFormulaImageURL', () => {
+  let image_url;
 
   beforeAll(async () => {
     jest.setTimeout(1000 * 20);
-    res = await evalOnGAS(function() {
-      var e = {
-        text: 'a + b > c & d'
-      };
-      return slackCommands.formula(e);
+    image_url = await evalOnGAS(function() {
+      return getFormulaImageURL('a + b < c & d');
     });
   });
 
-  it('returns response to channel with image attachment', () => {
-    expect(res.response_type).toBe('in_channel');
-    expect(res.attachments).toHaveLength(1);
-    const attachment = res.attachments[0];
-    expect(attachment).toHaveProperty('text');
-    expect(attachment.image_url).toBeTruthy();
-    expect(typeof attachment.image_url).toBe('string');
-  });
-
   it('returns available image_url', async () => {
-    const image_url = res.attachments[0].image_url;
     const { statusCode, headers, body } = await promisify(get)(image_url, undefined) as {statusCode: number, headers: {}, body: string};
     expect(statusCode).toBe(200);
     expect(headers['content-type']).toContain('image');
     expect(body).toBeTruthy();
     console.log(image_url);
+  });
+})
+
+describe('slackCommands.formula', () => {
+  beforeAll(async () => {
+    jest.setTimeout(1000 * 20);
+  });
+
+  it('returns response to raw TeX', async () => {
+    const res = await evalOnGAS(function() {
+      var e = {
+        text: 'a + b > c & d $ e'
+      };
+      return slackCommands.formula(e);
+    });
+    expect(res.response_type).toBe('in_channel');
+    expect(res.attachments).toHaveLength(1);
+    const attachment = res.attachments[0];
+    expect(attachment.image_url).toBeTruthy();
+    expect(typeof attachment.image_url).toBe('string');
+  });
+
+  it('returns response to text including $...$ and $$...$$ with post-text', async () => {
+    const res = await evalOnGAS(function() {
+      var e = {
+        text: 'abc $def$g h\ni$$j\nk$$lmn'
+      };
+      return slackCommands.formula(e);
+    });
+    expect(res.response_type).toBe('in_channel');
+    expect(res.attachments).toHaveLength(3);
+    expect(res.attachments[0].text).toBe('abc ');
+    expect(res.attachments[0].image_url).toContain(encodeURIComponent('$def$'));
+    expect(res.attachments[1].text).toBe('g h\ni');
+    expect(res.attachments[1].image_url).toContain(encodeURIComponent('$$j\nk$$'));
+    expect(res.attachments[2].text).toBe('lmn');
+    expect(res.attachments[2].image_url).toBeUndefined();
+  });
+
+  it('returns response to text including $...$ without post-text', async () => {
+    const res = await evalOnGAS(function() {
+      var e = {
+        text: 'abc $def$g h\ni$$j\nk$$'
+      };
+      return slackCommands.formula(e);
+    });
+    expect(res.response_type).toBe('in_channel');
+    expect(res.attachments).toHaveLength(2);
+    expect(res.attachments[0].text).toBe('abc ');
+    expect(res.attachments[0].image_url).toContain(encodeURIComponent('$def$'));
+    expect(res.attachments[1].text).toBe('g h\ni');
+    expect(res.attachments[1].image_url).toContain(encodeURIComponent('$$j\nk$$'));
   });
 });
 
--- src/commands.ts
+++ src/commands.ts
@@ -4,28 +4,56 @@ declare var global: any;
 
 global.slackCommands = {};
 
-export const formula = (params: SlackCommandParams): {} => {
+const getFormulaImageURL = (tex: string): string =>
+  'https://chart.googleapis.com/chart?cht=tx&chl=' +
+  encodeURIComponent(tex) +
+  '&ts=' + Date.now().toString();
+
+
+/// #if DEBUG
+global.getFormulaImageURL = getFormulaImageURL;
+/// #endif
+
+const formula = (params: SlackCommandParams): {} => {
   // if you want to change username or icon,
   // you should use postMessage instead.
-  const url =
-    'https://chart.googleapis.com/chart?cht=tx&chl=' +
-    encodeURIComponent(params.text) +
-    '&ts=' + Date.now().toString();
+  const re = /([^]*?)((\$\$?)[^]+?\3)/g;
+  const attachments = [];
+  let last_index = 0;
+  const attachment_color = '#ffffff';
+  while (1) {
+    const match = re.exec(params.text);
+    if (!match) break;
+    const [_, text, tex] = match;
+    last_index += _.length;
+    attachments.push({
+      text,
+      color: attachment_color,
+      image_url: getFormulaImageURL(tex)
+    });
+  }
+  if (last_index === 0) {
+    // regard full text as TeX
+    attachments.push({
+      color: attachment_color,
+      image_url: getFormulaImageURL(params.text)
+    });
+  } else if (last_index < params.text.length) {
+    // add strings left
+    attachments.push({
+      text: params.text.substr(last_index),
+      color: attachment_color
+    });
+  }
   return {
     response_type: 'in_channel',
-    attachments: [
-      {
-        text: '',
-        color: '#99ddff',
-        image_url: url
-      }
-    ]
+    attachments
   };
 };
 
 global.slackCommands.formula = formula;
 
-export const question = (params: SlackCommandParams): {} => {
+const question = (params: SlackCommandParams): {} => {
   // bot: @member @member @member
   const match = params.text.match(/<#(.+?)(\|.+)?>/);
   if (match) {
